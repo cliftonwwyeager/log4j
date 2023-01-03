d1c5ea8dfee05c9ceb3ea658e71b0bad1334000d
Clifton W. W. Yeager <55325353+cliftonwwyeager@users.noreply.github.com>
2023-01-03 15:46:12 -0600

    Add email logic

--- check-log4j-installations.ps1
+++ check-log4j-installations.ps1
@@ -1,1 +1,1 @@
-gci 'C:\' -rec -force -include *.jar -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -exp Path
+gci 'C:\' -rec -force -include *.jar -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -exp Path | send-mailmessage email@sender.com
