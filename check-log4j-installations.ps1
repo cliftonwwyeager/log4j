4317f7e0347fc8d35291ac1fe3c60b9d8811cd50
Clifton W. W. Yeager <55325353+cliftonwwyeager@users.noreply.github.com>
2023-01-03 17:57:28 -0600

    Fix syntax

--- check-log4j-installations.ps1
+++ check-log4j-installations.ps1
@@ -1,1 +1,1 @@
-gci 'C:\' -rec -force -include *.jar -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -exp Path | send-mailmessage email@sender.com
+gci 'C:\' -rec -force -include *.jar -ea 0 | foreach {select-string "JndiLookup.class" $_} | select -exp Path | Send-MailMessage -From 'Sender <sender@email.com>' -To 'Recipient <recipient@email.com>' -Subject 'Log4J Scan Results' -SmtpServer 'smtp.email.com'
